3398a1db4d6126443c612a63679f7ece4db55dd9
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2021-08-13 08:30:10 +0500

    lesson view

--- curriculum/views.py
+++ curriculum/views.py
@@ -14,3 +14,16 @@ class SubjectListView(DetailView):
     context_object_name = 'standards'
     model = Standard
     template_name = 'curriculum/subject_list_view.html'
+
+# darslar ro'yhati uchun view
+class LessonListView(DetailView):
+    context_object_name = 'subjects'
+    model = Subject
+    template_name = 'curriculum/lesson_list_view.html'
+
+
+# dars ma'lumotlari view si
+class LessonDetailView(DetailView):
+    context_object_name = 'lessons'
+    model = Lesson
+    template_name = 'curriculum/lesson_detail_view.html'
