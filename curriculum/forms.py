518943760130dba7b89fd10697d2ece293266b03
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2021-08-17 11:48:33 +0500

    comment reply

--- curriculum/forms.py
+++ curriculum/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lesson
+from .models import Lesson, Comment, Reply
 
 class LessonForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,28 @@ class LessonForm(forms.ModelForm):
                     "video", 
                     "ppt", 
                     "Notes")
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+        label = {"body": "Comment"}
+
+        widgets = {
+            'body':forms.Textarea(
+                attrs = {'class':'form-controll', 'rows': 4, 'cols': 70, 'pleaceholder':"Izoh yozing"}
+            )
+        }
+
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = Reply
+        fields = ('reply_body',)
+
+        widgets = {
+            'reply-body':forms.Textarea(
+                attrs = {'class':'form-controll', 'rows': 2, 'cols': 10, 'pleaceholder':"Javob yozing"}
+            )
+        }
--- curriculum/views.py
+++ curriculum/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.views.generic.edit import FormView
 from .models import Standard, Subject, Lesson
-from .forms import LessonForm
+from .forms import LessonForm, CommentForm, ReplyForm
 
 
 # Create your views here.
@@ -27,11 +28,67 @@ class LessonListView(DetailView):
 
 
 # dars ma'lumotlari view si
-class LessonDetailView(DetailView):
+class LessonDetailView(DetailView, FormView):
     context_object_name = 'lessons'
     model = Lesson
     template_name = 'curriculum/lesson_detail_view.html'
+    form_class = CommentForm
+    second_form_class = ReplyForm
 
+    def get_context_data(self, **kwargs):
+        context = super(LessonDetailView, self).get_context_data(**kwargs)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class()
+        # context['comments'] = Comment.objects.filter(id=self.object.id)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if 'form' in request.POST:
+            form_class = self.get_form_class()
+            form_name = 'form'
+        else:
+            form_class = self.second_form_class
+            form_name = 'form2'
+
+        form = self.get_form(form_class)
+        # print("the form name is : ", form)
+        # print("form name: ", form_name)
+        # print("form_class:",form_class)
+
+        if form_name == 'form' and form.is_valid():
+            print("comment form is returned")
+            return self.form_valid(form)
+        elif form_name == 'form2' and form.is_valid():
+            print("reply form is returned")
+            return self.form2_valid(form)
+
+    def get_success_url(self):
+        self.object = self.get_object()
+        standard = self.object.Standard
+        subject = self.object.subject
+        return reverse_lazy('curriculum:lesson_detail', kwargs={'standard': standard.slug,
+                                                                'subject': subject.slug,
+                                                                'slug': self.object.slug})
+
+    def form_valid(self, form):
+        self.object = self.get_object()
+        fm = form.save(commit=False)
+        fm.author = self.request.user
+        fm.lesson_name = self.object.comments.name
+        fm.lesson_name_id = self.object.id
+        fm.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form2_valid(self, form):
+        self.object = self.get_object()
+        fm = form.save(commit=False)
+        fm.author = self.request.user
+        fm.comment_name_id = self.request.POST.get('comment.id')
+        fm.save()
+        return HttpResponseRedirect(self.get_success_url())
 # darsni yaratish
 class LessonCreateView(CreateView):
     form_class = LessonForm
